fe7d7838c1228e8a814be83d3931951a45270429
Shaik Dawood <68695552+shaikDawood4@users.noreply.github.com>
2025-10-17 09:18:11 +0530

    Update index.js

--- init/index.js
+++ init/index.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose");
 const initdata = require("./data.js");
 const listing = require("../models/listing.js") 
 
-const MONGO_URL = "mongodb+srv://4dawoodshaik:dawoodshaik4@cluster0.re4l6cl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
+const MONGO_URL = process.env.ATLASDB_URL;
 main() .then(()=>console.log("connected to db"))
        .catch((err)=>{console.log(err)});
 
